d7cd9c97df996091f85b7954e841f77587c5427d
imbilgates <bilgates270703@gmail.com>
2025-07-10 10:18:21 +0530

    Update CORS middleware to include PATCH method in allowed HTTP methods

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     origin: process.env.CORS_ORIGIN || '*', // Allow all origins by default
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
 }));
 
 
