8f5546dad864ab6ee822527503ce6c07fbf2fa53
imbilgates <bilgates270703@gmail.com>
2025-07-10 09:55:16 +0530

    Update .gitignore to include .env and refactor vite.config.js for environment variable loading

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,21 +1,27 @@
 import path from "path"
-import tailwindcss from "@tailwindcss/vite"
+import { defineConfig, loadEnv } from "vite"
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import tailwindcss from "@tailwindcss/vite"
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000"
-      }
-    }
-  }
-})
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL || "http://localhost:3000",
+          changeOrigin: true,
+          secure: false
+        },
+      },
+    },
+  };
+});
